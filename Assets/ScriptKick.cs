7c791f0b280072a79ebc4810637342914dc675a5
saranpol <saran@hlpth.com>
2014-05-08 11:24:32 +0700

    first commit

---
+++ Assets/ScriptKick.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScriptKick : MonoBehaviour {
+
+	void OnMouseDown()
+	{
+		rigidbody.AddForce (transform.forward * 5, ForceMode.Impulse);
+		Debug.Log ("kick");
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/ScriptRestart.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScriptRestart : MonoBehaviour {
+
+	void OnMouseDown()
+	{
+		Debug.Log ("asdf");
+		Application.LoadLevel (Application.loadedLevelName);
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
